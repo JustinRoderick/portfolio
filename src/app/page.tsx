442410534a7905ac36af4944c9d2eee29241797d
JustinRoderick <justinroderick258@gmail.com>
2024-10-01 01:39:29 -0400

    Fix background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,5 @@ import Cursor from "@/components/cursor";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <body className="min-h-screen bg-blue-950">
-      {/*<Cursor />*/}
-
-      <div className=""></div>
-    </body>
-  );
+  return <body className="flex min-h-screen bg-blue-950"></body>;
 }
