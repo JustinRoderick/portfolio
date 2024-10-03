d05b2b457e20f907f1bcc97da424c67096492e3f
JustinRoderick <justinroderick258@gmail.com>
2024-10-02 23:40:15 -0400

    Add initial flex boxes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
-import Cursor from "@/components/cursor";
+//import Cursor from "@/components/cursor";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  return <body className="flex min-h-screen bg-blue-950"></body>;
+  return (
+    <body className="flex min-h-screen bg-blue-950 overflow-y-scroll">
+      <div className=" flex flex-1 flex-row items-center justify-center p-10 ">
+        <div className="flex flex-col"></div>
+        <div className="flex flex-col"></div>
+      </div>
+    </body>
+  );
 }
