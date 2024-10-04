861b39232c43110b0757f545396d17127e79774d
JustinRoderick <justinroderick258@gmail.com>
2024-10-03 23:54:44 -0400

    Added title and fixed spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,19 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
-    <body className="flex min-h-screen bg-blue-950 overflow-y-scroll">
+    <body className="flex min-h-screen overflow-y-scroll bg-blue-950">
       <div className=" flex flex-1 flex-row items-center justify-center p-10 ">
-        <div className="flex flex-col"></div>
-        <div className="flex flex-col"></div>
+        <div className="flex flex-col">
+          <div className="justify-center items-center p-3">
+            <h1 className="p-3 text-6xl text-white">Justin Roderick</h1>
+            <h2 className="p-3 text-3xl text-white">
+              Full-Stack Software Developer majoring in Computer Sciecne at UCF
+            </h2>
+          </div>
+        </div>
+        <div className="flex flex-col">
+          {/*<h1 className="p-3 text-3xl text-white">About</h1>*/}
+        </div>
       </div>
     </body>
   );
