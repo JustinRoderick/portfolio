1cb007641b2e70e93e67898052419dac87f0e23e
JustinRoderick <justinroderick258@gmail.com>
2024-12-07 23:11:46 -0500

    add some components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
+"use client";
 import Image from "next/image";
 //import Cursor from "@/components/cursor";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
-    <div className="flex flex-1 flex-row items-center justify-center ">
-      <section className=" flex flex-col ">
-        <div className="justify-center items-center p-3">
+    <div className="flex flex-row items-center justify-center">
+      <section className=" flex flex-col">
+        <div className="p-3">
           <h1 className="p-3 text-6xl text-white">Justin Roderick</h1>
           <h2 className="p-3 text-3xl text-white">
             Full-Stack Software Developer majoring in Computer Sciecne at UCF
           </h2>
         </div>
       </section>
-      <section className="flex flex-1 flex-row justify-center"></section>
+      <section className="flex flex-row justify-center"></section>
     </div>
   );
 }
