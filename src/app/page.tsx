f4d7a52dfec6d61f36a21b4522ccb6ebf0b7025a
JustinRoderick <justinroderick258@gmail.com>
2024-12-08 23:27:02 -0500

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,18 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   return (
-    <div className="flex flex-row items-center justify-center">
-      <section className=" flex flex-col">
-        <div className="p-3">
-          <h1 className="p-3 text-6xl text-white">Justin Roderick</h1>
-          <h2 className="p-3 text-3xl text-white">
-            Full-Stack Software Developer majoring in Computer Sciecne at UCF
-          </h2>
-        </div>
-      </section>
-      <section className="flex flex-row justify-center"></section>
+    <div className="h-screen snap-y snap-mandatory overflow-y-scroll">
+      <div className="flex flex-row items-center justify-center">
+        <section className=" flex flex-col">
+          <div className="items-start">
+            <h1 className="p-3 text-6xl text-white">Justin Roderick</h1>
+            <h2 className="p-3 text-3xl text-white">
+              Full-Stack Software Developer majoring in Computer Sciecne at UCF
+            </h2>
+          </div>
+        </section>
+        <section className="flex flex-row justify-center"></section>
+      </div>
     </div>
   );
 }
