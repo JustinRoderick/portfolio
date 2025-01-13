4e1bcb16724c48c275f97cd75ad48d50e070a08f
JustinRoderick <justinroderick258@gmail.com>
2025-01-13 02:40:46 -0500

    add template

---
+++ src/app/_components/nav.tsx
@@ -0,0 +1,3 @@
+export default function Nav() {
+  return <div className="flex flex-rox justify-between items-center"></div>;
+}
---
+++ src/consts/index.ts
@@ -0,0 +1,3 @@
+export const PROJECTS = {};
+
+export const EXPERIENCE = {};
