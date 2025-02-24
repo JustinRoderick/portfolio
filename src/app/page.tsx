8fa7b241ff30fa658cacb3ad7f246d05d9815f09
JustinRoderick <justinroderick258@gmail.com>
2025-02-23 23:54:30 -0500

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import About from "./_components/about";
 import Projects from "./_components/projects";
 import Experience from "./_components/experience";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 import { useEffect, useState, useRef } from "react";
 
@@ -17,17 +18,12 @@ export default function Home() {
   };
 
   return (
-    <div className="h-screen snap-y snap-mandatory overflow-y-scroll">
+    <div className="h-screen grid-cols-2 snap-y snap-mandatory overflow-y-scroll">
       <div className="flex flex-row items-center justify-center">
-        <section className=" flex flex-col">
-          <div className="items-start">
-            <h1 className="p-3 text-6xl text-white">Justin Roderick</h1>
-            <h2 className="p-3 text-3xl text-white">
-              Full-Stack Software Developer majoring in Computer Sciecne at UCF
-            </h2>
-          </div>
-        </section>
-        <section className="flex flex-row justify-center"></section>
+        <div className=" flex-1 flex-col col-span-1">
+        </div>
+        <div className="flex-1 flex-col col-span-1">
+        </div>
       </div>
     </div>
   );
