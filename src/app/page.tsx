4502aa0fdf2fe76b4ee46334821465c7565c5307
JustinRoderick <justinroderick258@gmail.com>
2025-02-24 00:29:34 -0500

    start spotlight effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-"use client";
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
 //import Cursor from "@/components/cursor";
-import About from "./_components/about";
-import Projects from "./_components/projects";
-import Experience from "./_components/experience";
-import { ScrollArea } from "@/components/ui/scroll-area";
+import About from './_components/about';
+import Projects from './_components/projects';
+import Experience from './_components/experience';
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Spotlight } from './_components/spotlight';
 
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from 'react';
 
 export default function Home() {
   const aboutRef = useRef<HTMLDivElement | null>(null);
@@ -14,16 +15,23 @@ export default function Home() {
   const experienceRef = useRef<HTMLDivElement | null>(null);
 
   const scrollToSection = (ref: React.RefObject<HTMLDivElement>) => {
-    ref.current?.scrollIntoView({ behavior: "smooth" });
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
   return (
-    <div className="h-screen grid-cols-2 snap-y snap-mandatory overflow-y-scroll">
-      <div className="flex flex-row items-center justify-center">
-        <div className=" flex-1 flex-col col-span-1">
-        </div>
-        <div className="flex-1 flex-col col-span-1">
-        </div>
+    <div className="h-screen grid grid-cols-2">
+      <div className="h-full flex flex-col items-center justify-center">
+        <Spotlight>
+          <h1>Justin Roderick</h1>
+          <p>Software Engineering Student at UCF</p>
+        </Spotlight>
+      </div>
+      <div className="h-full snap-y snap-mandatory overflow-y-scroll flex flex-col items-center justify-center">
+        <ScrollArea className="w-full h-full">
+          <About />
+          <Projects />
+          <Experience />
+        </ScrollArea>
       </div>
     </div>
   );
