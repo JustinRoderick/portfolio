2b11b95367accc9bf890700540cbf179d97c6c71
JustinRoderick <justinroderick258@gmail.com>
2025-02-25 23:17:50 -0500

    format

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,35 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Justin Roderick",
+  title: 'Justin Roderick',
   description:
-    "Justin Roderick is an aspiring software engineer and current student at the University of Central Florida.",
+    'Justin Roderick is an aspiring software engineer and current student at the University of Central Florida.',
   keywords: [
-    "Justin Roderick",
-    "UCF",
-    "Software Engineer",
-    "Web Developer",
-    "Developer",
-    "Computer Science",
-    "Engineer",
-    "Justin",
-    "Roderick",
-    "University of Central Florida",
-    "Software",
+    'Justin Roderick',
+    'UCF',
+    'Software Engineer',
+    'Web Developer',
+    'Developer',
+    'Computer Science',
+    'Engineer',
+    'Justin',
+    'Roderick',
+    'University of Central Florida',
+    'Software',
   ],
   openGraph: {
-        type: "website",
-        title: "Justin Roderick",
-        description:
-            "Justin Roderick is an aspiring software engineer and current student at the University of Central Florida.",
-        url: "https://justinroderick.dev",
-    },
+    type: 'website',
+    title: 'Justin Roderick',
+    description:
+      'Justin Roderick is an aspiring software engineer and current student at the University of Central Florida.',
+    url: 'https://justinroderick.dev',
+  },
   icons: {
-    icon: "/favicon.ico",
+    icon: '/favicon.ico',
   },
 };
 
@@ -42,7 +42,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={
-          "flex min-h-full h-screen overflow-hidden flex-col bg-cover bg-blue-950 antialiased"
+          'flex min-h-full h-screen overflow-hidden flex-col bg-cover bg-blue-950 antialiased'
         }
       >
         {children}
