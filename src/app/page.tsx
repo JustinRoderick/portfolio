5709b1f268392d202b03ee3721bdc84bbf2c83f6
JustinRoderick <justinroderick258@gmail.com>
2025-02-27 23:12:47 -0500

    set scroll refs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Projects from './_components/projects';
 import Experience from './_components/experience';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { Spotlight } from './_components/spotlight';
+import { Button } from '@/components/ui/button';
 
 import { useEffect, useState, useRef } from 'react';
 
@@ -28,13 +29,36 @@ export default function Home() {
         <p className="text-xl text-white w-full text-center animate-fade-down">
           Software Engineering Student at UCF
         </p>
+        <div className="flex flex-col gap-4 mt-8">
+          <Button variant="outline" onClick={() => scrollToSection(aboutRef)}>
+            About
+          </Button>
+          <Button
+            variant="outline"
+            onClick={() => scrollToSection(projectsRef)}
+          >
+            Projects
+          </Button>
+          <Button
+            variant="outline"
+            onClick={() => scrollToSection(experienceRef)}
+          >
+            Experience
+          </Button>
+        </div>
         {/* </Spotlight> */}
       </div>
       <div className="h-full snap-y snap-mandatory overflow-y-scroll flex flex-col items-center justify-center">
         <ScrollArea className="w-full h-full">
-          <About />
-          <Projects />
-          <Experience />
+          <div ref={aboutRef}>
+            <About />
+          </div>
+          <div ref={projectsRef}>
+            <Projects />
+          </div>
+          <div ref={experienceRef}>
+            <Experience />
+          </div>
         </ScrollArea>
       </div>
     </div>
