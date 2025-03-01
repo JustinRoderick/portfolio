73a35e29b3fde2adb43a57bd3e7494aceda4c39a
JustinRoderick <justinroderick258@gmail.com>
2025-02-28 23:45:05 -0500

    import

--- src/app/_components/experience.tsx
+++ src/app/_components/experience.tsx
@@ -1,4 +1,11 @@
 import { experience } from '@/consts/index';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 export default function Experience() {
   return <div className="h-screen"></div>;
