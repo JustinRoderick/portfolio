ccac2997151dc36401fb3f944e4c68feac468ee9
JustinRoderick <justinroderick258@gmail.com>
2025-09-27 10:09:42 -0400

    add _break to links

--- src/app/_components/content.tsx
+++ src/app/_components/content.tsx
@@ -116,23 +116,23 @@ export default function Content() {
             </Button>
           </div>
 
-          {/* <div className="mt-12 lg:mt-20 flex flex-row gap-6 p-4 items-center justify-center">
-            <Link href="https://github.com/justinroderick">
+          <div className="mt-12 lg:mt-20 flex flex-row gap-6 p-4 items-center justify-center">
+            <Link href="https://github.com/justinroderick" target="_blank">
               <div className="w-16 h-16 lg:w-24 lg:h-24 transition-all duration-300 ease-in-out hover:scale-110">
                 <CustomCanvas svgPath="/github.svg" />
               </div>
             </Link>
-            <Link href="https://linkedin.com/in/justinroderick">
+            <Link href="https://linkedin.com/in/justinroderick" target="_blank">
               <div className="w-16 h-16 lg:w-24 lg:h-24 transition-all duration-300 ease-in-out hover:scale-110">
                 <CustomCanvas svgPath="/linkedin.svg" />
               </div>
             </Link>
-            <Link href="https://justinroderick.dev/resume.pdf">
+            <Link href="https://justinroderick.dev/resume.pdf" target="_blank">
               <div className="w-16 h-16 lg:w-24 lg:h-24 transition-all duration-300 ease-in-out hover:scale-110">
                 <CustomCanvas svgPath="/resume.svg" />
               </div>
             </Link>
-          </div> */}
+          </div>
         </div>
       </div>
 
