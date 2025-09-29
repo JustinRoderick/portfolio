281bb523fc7bc58c2e19ed2a82fb2c7783be6c4c
JustinRoderick <justinroderick258@gmail.com>
2025-09-29 12:52:58 -0400

    downgrade next.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['three'],
+  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
 };
 
 export default nextConfig;
