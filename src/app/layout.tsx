92c1c598b250344d4b04ad1023b685ecb182904d
JustinRoderick <justinroderick258@gmail.com>
2026-03-17 22:52:03 -0400

    opengraph image and jsonLd

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import SpotlightProvider from './_components/spotlight-provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://justinroderick.dev'),
   title: 'Justin Roderick',
   description:
     'Justin Roderick is an aspiring software engineer and current student at the University of Central Florida.',
@@ -29,6 +30,7 @@ export const metadata: Metadata = {
     description:
       'Justin Roderick is an aspiring software engineer and current student at the University of Central Florida.',
     url: 'https://justinroderick.dev',
+    images: ['/portfolio.png'],
   },
   icons: {
     icon: '/favicon.ico',
@@ -40,6 +42,18 @@ export const viewport: Viewport = {
   initialScale: 1,
 };
 
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'Person',
+  name: 'Justin Roderick',
+  url: 'https://justinroderick.dev',
+  jobTitle: 'Software Engineer',
+  alumniOf: {
+    '@type': 'CollegeOrUniversity',
+    name: 'University of Central Florida',
+  },
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -47,6 +61,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+      </head>
       <body
         className={
           'flex min-h-full h-screen lg:overflow-hidden flex-col bg-cover bg-purple-950 antialiased'
