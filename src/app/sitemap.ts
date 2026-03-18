6ce65a2225bea79b3adae857ea48a41584dc55f7
JustinRoderick <justinroderick258@gmail.com>
2026-03-17 22:51:38 -0400

    remove dead routes

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -7,15 +7,5 @@ export default function sitemap(): MetadataRoute.Sitemap {
       lastModified: new Date(),
       priority: 1,
     },
-    {
-      url: 'https://justinroderick.dev/experience',
-      lastModified: new Date(),
-      priority: 0.9,
-    },
-    {
-      url: 'https://justinroderick.dev/projects',
-      lastModified: new Date(),
-      priority: 0.9,
-    },
   ];
 }
